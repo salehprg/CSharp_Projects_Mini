c8abd3a445173bef3284ab4fbc56132c044a8eda
Saleh <salehebrahimian@gmail.com>
2023-08-21 08:43:00 +0330

    Delete Artan Remastered directory

--- Artan Remastered/Models/UserDataVW.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-using artan_gym.Models;
-using Models.User;
-
-public class UserDataVW {
-    public List<MealView> userMealView {get;set;}
-    public List<ExerciseView> userExreciseView {get;set;}
-    public UserModel userInfo {get;set;}
-}
